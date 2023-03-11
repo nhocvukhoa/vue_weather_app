7cc3d4b935aace15865fa2bb12e5d4cec9fb7689
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-03-11 22:27:58 +0700

    create navigation

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="flex flex-col min-h-screen bg-weather-primary">
+    <SiteNavigation></SiteNavigation>
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SiteNavigation from "./components/SiteNavigation.vue";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    SiteNavigation,
+  },
+};
+</script>
